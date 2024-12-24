190aaf9240af35c74015e05fc49b9df4aef60fb9
Vultys <vlad.serdiuk750@gmail.com>
2024-12-24 15:50:55 +0200

    Lecture #2.3. Reading Input.

---
+++ MPCourse_Game/Assets/Scripts/Input/InputReader.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static Controls;
+
+[CreateAssetMenu(fileName = "New Input Reader", menuName = "Input/Input Reader")]
+public class InputReader : ScriptableObject, IPlayerActions
+{
+    public event Action<bool> PrimaryFireEvent;
+
+    public event Action<Vector2> MoveEvent;
+
+    private Controls _controls;
+
+    private void OnEnable() 
+    {
+        if(_controls == null)
+        {
+            _controls = new Controls();
+            _controls.Player.SetCallbacks(this);
+        }
+
+        _controls.Player.Enable();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        MoveEvent?.Invoke(context.ReadValue<Vector2>());
+    }
+
+    public void OnPrimaryFire(InputAction.CallbackContext context)
+    {
+        PrimaryFireEvent?.Invoke(context.performed);
+    }
+}
