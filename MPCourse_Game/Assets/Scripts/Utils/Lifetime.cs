2b7d8dc1f1384214c83a5208e77460a8eb370627
Vultys <vlad.serdiuk750@gmail.com>
2024-12-25 20:04:25 +0200

    Lecture #2.7. Networked Projectiles

---
+++ MPCourse_Game/Assets/Scripts/Utils/Lifetime.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Lifetime : MonoBehaviour
+{
+    [SerializeField] private float _lifeTime = 1f;
+
+    private void Start() 
+    {
+        Destroy(gameObject, _lifeTime);
+    }
+}
